1fab4630d8d8d7e3c93fc8c1012d3288453cbdbf
orochi69 <alerncheruiyot@gmail.com>
2024-10-02 00:20:35 +0300

    fixed the gender field from inserting dropdown title to db as a value

--- resources/views/pages/registeredcustomers/updatekyc.blade.php
+++ resources/views/pages/registeredcustomers/updatekyc.blade.php
@@ -48,14 +48,7 @@
                                             <label class="form-label" for="form3Example1c">ID Number</label>
                                         </div>
                                     </div>
-
-                                    <div class="d-flex flex-row align-items-center mb-4">
-                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                        <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                            <input type="text" id="form3Example1c" value="{{$loggedinuserdetails->gender}}" name='gender' class="form-control"/>
-                                            <label class="form-label" for="form3Example1c">Gender</label>
-                                        </div>
-                                    </div>
+                                    
 
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
@@ -63,7 +56,7 @@
                                             <label class="form-label" for="form3Example1c">Gender</label>
                                             <select class="form-select" aria-label="Default select example"
                                                     name='gender'>
-                                                <option selected>Select Gender</option>
+                                                <option selected>{{$loggedinuserdetails->gender}}</option>
                                                 <option value="Male">Male</option>
                                                 <option value="Female">Female</option>
                                                 <option value="LGBTQIH+">LGBTQIH+</option>
--- resources/views/pages/registeredcustomers/updatekycnew.blade.php
+++ resources/views/pages/registeredcustomers/updatekycnew.blade.php
@@ -49,13 +49,6 @@
                                         </div>
                                     </div>
 
-                                    <div class="d-flex flex-row align-items-center mb-4">
-                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                        <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                            <input type="text" id="form3Example1c"  name='gender' class="form-control"/>
-                                            <label class="form-label" for="form3Example1c">Gender</label>
-                                        </div>
-                                    </div>
 
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
@@ -63,7 +56,6 @@
                                             <label class="form-label" for="form3Example1c">Gender</label>
                                             <select class="form-select" aria-label="Default select example"
                                                     name='gender'>
-                                                <option selected>Select Gender</option>
                                                 <option value="Male">Male</option>
                                                 <option value="Female">Female</option>
                                                 <option value="LGBTQIH+">LGBTQIH+</option>
